d5ee9da8223fabcc7d29b26d6ee1c75342667ef9
wimjjj <wim.jongeneel1@gmail.com>
2016-12-16 16:52:27 +0100

    added pagination

--- src/pages/edit.vue
+++ src/pages/edit.vue
@@ -1,20 +1,19 @@
 <template>
     <main-layout>
-        <v-link :href=links.self.href><span class="glyphicon glyphicon-circle-arrow-left"></span> blog</v-link>
         <form>
             <div class="form-group" onsubmit="return false">
                 <label for="title">title</label>
-                <input type="text" class="form-control" id="title" :v-model="blog.title" :value="blog.title">
+                <input type="text" class="form-control" id="title" v-model="blog.title">
             </div>
             <div class="form-group">
                 <label for="author">author:</label>
-                <input type="text" class="form-control" id="author" :v-model=blog.author :value="blog.author">
+                <input type="text" class="form-control" id="author" v-model="blog.author">
             </div>
             <div class="form-group">
                 <label for="body">body:</label>
-                <textarea class="form-control" rows="5" id="body" :v-model="blog.body" :value="blog.body"></textarea>
+                <textarea class="form-control" rows="5" id="body" v-model="blog.body"></textarea>
             </div>
-            <button type="button" class="btn btn-default" v-on:click="update">Submit</button>
+            <button type="button" class="btn btn-default"  v-on:click="patch">Submit</button>
         </form>
     </main-layout>
 </template>
@@ -22,44 +21,39 @@
 <script>
 import MainLayout from '../layouts/Main.vue'
 import VLink from '../components/VLink.vue'
+import page from 'page'
 
 export default{
     data () {
         return {
-            blog_id: window.blog_id,
             blog: {
                 title: '',
-                author: '',
-                body: ''
-            },
-            links: {
-                self: {
-                    href: '/' + window.blog_id
-                }
+                body: '',
+                author: ''
             }
         }
     },
+    components:{
+        MainLayout,
+        VLink
+    },
     beforeMount () {
-        this.$http.get('http://145.24.222.128/' + this.$data.blog_id).then(function(data){
+        this.$http.get('http://145.24.222.128/' + window.blog_id).then(function(data){
             this.$set(this, 'blog', data.body);
         }.bind(this));
-   },
-   methods: {
-        update: function(){
-            this.$http.post('http://145.24.222.128/' + this.blog_id + '?_method=PATCH',
+    },
+    methods: {
+        patch: function(){
+            this.$http.post('http://145.24.222.128/' + window.blog_id + '?_method=PATCH',
                 {
                     'title': this.blog.title,
                     'author': this.blog.author,
                     'body': this.blog.body
-                }).then(function(data){
-                this.$set(this, 'blog', data.body);
-                console.log(data);
+                }
+            ).then(function(data){
+                page('/' + String(data.body.id));
             }.bind(this));
         }
-   },
-   components:{
-       MainLayout,
-       VLink
    }
 }
 </script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,12 +1,22 @@
 <template>
     <main-layout>
         <v-link href="/new"><span class="glyphicon glyphicon-plus"></span> new</v-link>
+
+        <div id="links" class="btn-group pull-right">
+            <button type="button" class="btn btn-default" v-on:click=first><span class="glyphicon glyphicon-step-backward"></span></button>
+            <button type="button" class="btn btn-default" v-on:click=prevv><span class="glyphicon glyphicon-arrow-left"></span></button>
+            <button type="button" class="btn btn-default" v-on:click=next><span class="glyphicon glyphicon-arrow-right"></span></button>
+            <button type="button" class="btn btn-default" v-on:click=last><span class="glyphicon glyphicon-step-forward"></span></button>
+        </div>
+
         <div class="list-group">
-            <v-link  v-for="blog in blogs" :href=String(blog.id) class="list-group-item">
+            <v-link v-for="blog in blogs" :href=String(blog.id) class="list-group-item">
                 <h1>{{ blog.title }}</h1>
                 <h5>written by: {{ blog.author }}</h5>
             </v-link>
         </div>
+
+
     </main-layout>
 </template>
 
@@ -17,13 +27,42 @@ import VLink from '../components/VLink.vue'
 export default {
    data () {
         return {
-            'blogs': ''
+            blogs: '',
+            links: {}
         };
    },
    beforeMount () {
-        this.$http.get('http://145.24.222.128').then(function(data){
+        this.$http.get('http://145.24.222.128?limit=5').then(function(data){
             this.$set(this, 'blogs', data.body.items);
+            this.$set(this, 'links', data.body.pagination._links);
+            console.log(this.links);
         }.bind(this));
+   },
+   methods: {
+        first: function(){
+            this.$http.get(this.links.first.href).then(function(data){
+                this.$set(this, 'blogs', data.body.items);
+                this.$set(this, 'links', data.body.pagination._links)
+            }.bind(this));
+        },
+        prevv: function(){
+            this.$http.get(this.links.previous.href).then(function(data){
+                this.$set(this, 'blogs', data.body.items);
+                this.$set(this, 'links', data.body.pagination._links);
+            }.bind(this));
+        },
+        next: function(){
+            this.$http.get(this.links.next.href).then(function(data){
+                this.$set(this, 'blogs', data.body.items);
+                this.$set(this, 'links', data.body.pagination._links);
+            }.bind(this));
+        },
+        last: function(){
+            this.$http.get(this.links.last.href).then(function(data){
+                this.$set(this, 'blogs', data.body.items);
+                this.$set(this, 'links', data.body.pagination._links);
+            }.bind(this));
+        }
    },
    components: {
        MainLayout,
@@ -42,5 +81,9 @@ export default {
 
     h1 {
         margin-top: -5px;
+    }
+
+    .list-group {
+        margin-top: 20px;
     }
 </style>
